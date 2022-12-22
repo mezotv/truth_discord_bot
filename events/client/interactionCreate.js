6bafc351dd9938c844bc95110be604b8ae8cfdb3
Dominik K <dominik@koch-bautechnik.de>
2022-12-23 00:45:55 +0100

    Revert "Fix interactionCreate.js line 3"
    This reverts commit 9c146e846ea70c08973ce1fb4a2522589c323860.

--- events/client/interactionCreate.js
+++ events/client/interactionCreate.js
@@ -1,30 +1,15 @@
 module.exports = (client, interaction) => {
-  if (!interaction.guild) {
-    if (interaction.isChatInputCommand()) {
-      const command = client.commands.get(interaction.commandName);
-      if (!command) return;
-      try {
-        command.execute(interaction, client);
-      } catch (err) {
-        if (err) console.error(err);
-        interaction.reply({
-          content: "An error occurred while trying to execute that command.",
-          ephemeral: true,
-        });
-      }
-    };
-  } else {
-    if (interaction.isChatInputCommand()) {
-      const command = client.commands.get(interaction.commandName);
-      if (!command) return;
-      try {
-        command.execute(interaction, client);
-      } catch (err) {
-        if (err) console.error(err);
-        interaction.reply({
-          content: "An error occurred while trying to execute that command.",
-          ephemeral: true,
-        });
-      }
+    if (interaction.isChatInputCommand())
+    const command = client.commands.get(interaction.commandName);
+    if (!command) return;
+    try {
+      command.execute(interaction, client);
+    } catch (err) {
+      if (err) console.error(err);
+      interaction.reply({
+        content: "An error occurred while executing that command.",
+        ephemeral: true,
+      });
+    }
   };
   
